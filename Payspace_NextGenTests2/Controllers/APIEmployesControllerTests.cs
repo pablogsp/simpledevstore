508793c275581b9a413e763af11aa95f5c5e32bf
Pablo Gomes Souza Pinto <pablogsp@gmail.com>
2020-03-11 17:18:40 -0300

    Implementação

--- Payspace_NextGenTests2/Controllers/APIEmployesControllerTests.cs
+++ Payspace_NextGenTests2/Controllers/APIEmployesControllerTests.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Payspace_NextGen.Controllers;
+using Payspace_NextGen;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +14,21 @@ namespace Payspace_NextGen.Controllers.Tests
         [TestMethod()]
         public async Task APIEmployesControllerTestAsync()
         {
+
+            APIcalculationDetailsController ap = new APIcalculationDetailsController(new Models.localdbEntities());
+            decimal teste = ap.GetcalculationResultRateNoProgressive(new Models.Employe()
+            {
+                CPF = "0000000",
+                Name = "CalcTestePaySpace",
+                PostCode = new Models.PostCode()
+                {
+                    PostCodeName = "TaxTeste PaySpace",
+                    Id = "",
+                    TaxRate = "Progressive",
+                    Type = new Models.Type() { TypeRate = "Progressive" },
+                    
+                } });
+
             Assert.Fail();
         }
     }
