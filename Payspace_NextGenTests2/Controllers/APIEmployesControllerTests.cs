1ebb8625ecc23035cf240273826d28fbfa6e0c67
Pablo Gomes Souza Pinto <pablogsp@gmail.com>
2020-03-11 11:47:26 -0300

    carla

---
+++ Payspace_NextGenTests2/Controllers/APIEmployesControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Payspace_NextGen.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payspace_NextGen.Controllers.Tests
+{
+    [TestClass()]
+    public class APIEmployesControllerTests
+    {
+        [TestMethod()]
+        public async Task APIEmployesControllerTestAsync()
+        {
+            Assert.Fail();
+        }
+    }
+}
